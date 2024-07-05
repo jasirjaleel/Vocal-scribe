6674e484167baebf7bb4ca9d19f5dabd74c3a9de
JR0-7 <jasirjaleel777@gmail.com>
2024-07-05 09:59:28 +0530

    removed: base_url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='flex items-center justify-between gap-4 p-4'>
-        <a href={import.meta.env.BASE_URL}>
+        <a href='/'>
             <h1 className='font-medium'>Vocal<span className='text-blue-400 bold'>Scribe</span></h1>
         </a>
-        <a href={import.meta.env.BASE_URL} className='flex items-center gap-2 specialBtn px-3 text-sm py-2 rounded-lg text-blue-400'>
+        <a href='/' className='flex items-center gap-2 specialBtn px-3 text-sm py-2 rounded-lg text-blue-400'>
             <p>New</p>
             <i className='fa-thin fa-plus'></i>
         </a>
